6b3098adb1bf41b65673b25540be5f9a54bddb80
Turbobot <turbobot@vercel.com>
2025-10-20 12:38:42 +0700

    feat(create-turbo): apply official-starter transform

--- src/store/tags.ts
+++
@@ -1,6 +0,0 @@
-export const topics: string[] = [
-    "Mathematics for Machine Learning",
-    "Building a Neural Network from Scratch",
-    "Building a Faster R-CNN from Scratch",
-    "Introduce to Data Structures and Algorithms",
-]
