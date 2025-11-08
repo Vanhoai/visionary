1e5aa532a43f180b9fb31a86147764cd102495c1
Hinsun <vanhoai.adv@gmail.com>
2025-11-08 21:37:52 +0700

    feat: add EditorConfig and GitHub Actions workflow for deployment, update Next.js configuration

--- components/shared/layouts/main/header.tsx
+++ components/shared/layouts/main/header.tsx
@@ -3,8 +3,6 @@
 import * as React from "react"
 import Link from "next/link"
 import Image from "next/image"
-import { ChevronDown, CircleCheckIcon, CircleHelpIcon, CircleIcon } from "lucide-react"
-
 import { useIsMobile } from "@/hooks"
 import {
     Avatar,
--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
+    output: "export",
     images: {
         domains: ["i.pinimg.com"],
+        unoptimized: true,
     },
+    basePath: process.env.NODE_ENV === "production" ? "/visionary" : "",
+    assetPrefix: process.env.NODE_ENV === "production" ? "/visionary/" : "",
 }
 
 export default nextConfig
